4215569052d899d31dd2774704401ac618dd2794
Andry-Kodzhebash <andrii.kodzhebash@gmail.com>
2025-03-16 21:35:07 +0100

    update

--- level_3/app.py
+++ level_3/app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 tasks = []
-app.route()
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form.get('title')
+    if title:
+        task = {'title': title, 'completed': False}
+        tasks.append(task)
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<int:task_index>', methods=['GET'])
+def complete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        tasks[task_index]['completed'] = True
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_index>', methods = ['GET'])
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        tasks.pop(task_index)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
